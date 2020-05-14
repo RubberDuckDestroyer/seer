9a1e42e078aad07304ee2d9108a05da1f468e82d
Jerry Kim <jerryrox@live.co.kr>
2020-05-14 18:51:49 +1200

    Implemented test data addition endpoint

--- routes/api/backend.js
+++ routes/api/backend.js
@@ -1,15 +1,37 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
+const BibtexParser = require("../../libs/formats/BibtexParser");
+
+const Article = require("../../models/Article");
 
 const router = express.Router();
 
+// @route    GET api/backend/fill-test-data
+// @desc     Automate filling of test data to the database.
 // @access   Public
+router.get("/fill-test-data", async (req, res) => {
     try {
-        await Promise.all(requests);
-        res.send("Enums inserted. Check server logs for any information.");
+        const testRawEntries = fs.readFileSync(path.join(__dirname, "TestTddEntries.txt"), "utf8");
+        const entries = BibtexParser.parse(testRawEntries);
+
+        const saves = [];
+        entries.forEach(async (e) => {
+            const article = new Article({
+                submission: {
+                    bibliography: e
+                }
+            });
+            saves.push(article.save());
+        });
+
+        await Promise.all(saves);
+        res.send("Inserted test data to database.");
     }
-    catch (yieldError) {
-        res.status(401).send(`Failed while waiting for insertion: ${yieldError}`);
+    catch (e) {
+        console.log(e);
+        res.status(401).send(`Error while filling test data: ${e}`);
     }
 });
 
