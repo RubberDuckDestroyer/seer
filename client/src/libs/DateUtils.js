8392f85cdd8b2dfd08d6639357b2b45997b6c091
Jerry Kim <jerryrox@live.co.kr>
2020-05-22 16:53:19 +1200

    Lint fix

--- client/src/libs/DateUtils.js
+++ client/src/libs/DateUtils.js
@@ -7,6 +7,15 @@ const DateUtils = {
         }
         const newDate = moment(date).utcOffset("+0000").hour(0).minute(0).second(0);
         return newDate.toDate();
+    },
+
+    monthNames: [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ],
+
+    getMonthName: (monthIndex) => {
+        return DateUtils.monthNames[monthIndex];
     }
 };
 export default DateUtils;
