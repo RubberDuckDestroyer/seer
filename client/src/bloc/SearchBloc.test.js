b7034680995d4b9302580521f45dd1c50a2017d1
Jerry Kim <jerryrox@live.co.kr>
2020-05-27 18:32:24 +1200

    Configured client test library to run in CI mode.

--- client/src/bloc/SearchBloc.test.js
+++ client/src/bloc/SearchBloc.test.js
@@ -1,13 +1,13 @@
 import SearchBloc, { SearchFilterInfo } from "./SearchBloc";
 
-test("Tests SearchBloc date range", () => {
+test("Change date range", () => {
     const bloc = new SearchBloc();
     bloc.minDate.setValue("2020-05-10");
     bloc.maxDate.setValue("2020-06-11");
     expect(bloc.minDate.getValue()).toBe("2020-05-10");
     expect(bloc.maxDate.getValue()).toBe("2020-06-11");
 });
-test("Tests adding new filters", () => {
+test("Adding new filters", () => {
     const bloc = new SearchBloc();
     expect(bloc.filters.getValue().length).toBe(1);
     expect(bloc.joints.getValue().length).toBe(0);
@@ -17,8 +17,8 @@ test("Tests adding new filters", () => {
     expect(bloc.joints.getValue().length).toBe(1);
 
     const firstFilter = bloc.filters.getValue()[0];
-    const firstJoint = bloc.joints.getValue()[0];
     bloc.removeFilter(firstFilter)
     expect(bloc.filters.getValue().length).toBe(1);
     expect(bloc.joints.getValue().length).toBe(0);
+    expect(bloc.filters.getValue()[0]).not.toEqual(firstFilter);
 });
