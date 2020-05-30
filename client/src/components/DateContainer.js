55a77df21f541b4f45ce6caf3911a346686d5e87
Jerry Kim <jerryrox@live.co.kr>
2020-05-30 16:58:25 +1200

    Revert "Merge branch 'feature/result-table-change' of https://github.com/RubberDuckDestroyer/seer into feature/result-table-change"
    This reverts commit e32b606d150d84a9be2b5acfe2d2b15700b88811, reversing
    changes made to 92d1b1eb3849a6fd9032ee86ef3177f037189d43.

--- client/src/components/DateContainer.js
+++ client/src/components/DateContainer.js
@@ -76,77 +76,71 @@ const DateContainer = ({ style }) => {
     }
   };
 
-      const formatDateLabel = (i) => {
-        if (i >= 0 && i <= 11) {
-          return DateUtils.getMonthName(i).substr(0, 3);
-        }
-        return "";
-        const onFilterDateButton = () => {
-          searchBloc.filterDate.setValue(!searchBloc.filterDate.getValue());
-        }; 
+  const onFilterDateButton = () => {
+    searchBloc.filterDate.setValue(!searchBloc.filterDate.getValue());
+  };
 
-    
   const classes = useStyles();
 
-        return (
-          <Container
+  return (
+    <Container
       style={{
         ...style,
         borderRadius: "10px",
       }}
     >
-          <Grid className={classes.gridContainer} container spacing={1}>
+      <Grid className={classes.gridContainer} container spacing={1}>
         <Grid item xs={4} md={3} lg={2}>
-          < TextField
+          <TextField
             id="startDate"
             label="From Date"
             type="date"
             value={minDateString}
             onChange={onMinDateChange}
             className={classes.selectionItem}
-              InputLabelProps={{
+            InputLabelProps={{
               shrink: true,
             }}
-            />
+          />
         </Grid>
-          <Hidden smDown>
+        <Hidden smDown>
           <Grid item md={3} lg={4}>
-          <Typography id="year-range-slider" gutterBottom >
+            <Typography id="year-range-slider" gutterBottom >
               Year
             </Typography>
             <Slider
               style={{ width: "90%", marginLeft: "5%" }}
-          min={1900}
-          max={new Date().getUTCFullYear()}
-          value={[minDate.getUTCFullYear(), maxDate.getUTCFullYear()]}
+              min={1900}
+              max={new Date().getUTCFullYear()}
+              value={[minDate.getUTCFullYear(), maxDate.getUTCFullYear()]}
               onChange={onYearChange}
-          valueLabelDisplay="auto"
-          aria-labelledby="range-slider"
-        />
-          </Grid>  
-    <Grid item md={3} lg={4}>
-            <Typography id="month-range-slider" gutterBottom>
-        Month
-            </Typo g raphy>
-  < Slider  
-  style={{ width: "90%", marginLeft: "5%" }}
-              mi n={0}
-              ma x={11}
-        value={[minDate.getUTCMonth(), maxDate.getUTCMonth()]}
-        onChange={onMonthChange}
-        valueLabelDisplay="auto"
-        aria-labelledby="range-slider"
-        valueLabelFormat={formatDateLabel}
-      />
+              valueLabelDisplay="auto"
+              aria-labelledby="range-slider"
+            />
           </Grid>
-        </Hidden>  
-<Grid item xs={4} md={3} lg={2}>
-          < TextField
+          <Grid item md={3} lg={4}>
+            <Typography id="month-range-slider" gutterBottom>
+              Month
+            </Typography>
+            <Slider
+              style={{ width: "90%", marginLeft: "5%" }}
+              min={0}
+              max={11}
+              value={[minDate.getUTCMonth(), maxDate.getUTCMonth()]}
+              onChange={onMonthChange}
+              valueLabelDisplay="auto"
+              aria-labelledby="range-slider"
+              valueLabelFormat={(i) => DateUtils.getMonthName(i).substr(0, 3)}
+            />
+          </Grid>
+        </Hidden>
+        <Grid item xs={4} md={3} lg={2}>
+          <TextField
             id="endDate"
-  label="To Date"
-            ty pe="date"
-             value={maxDateString}
-            onC hange={onMaxDateChange}
+            label="To Date"
+            type="date"
+            value={maxDateString}
+            onChange={onMaxDateChange}
             className={classes.selectionItem}
             InputLabelProps={{
               shrink: true,
