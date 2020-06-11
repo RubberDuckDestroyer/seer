b6a02b722501f9fa526b465feac8612893c78208
Jerry Kim <jerryrox@live.co.kr>
2020-06-11 16:31:03 +1200

    Added article submission route

--- src/routes/api/article.js
+++ src/routes/api/article.js
@@ -3,6 +3,10 @@ import express from "express";
 import Article from "../../models/Article";
 import QueryFilterBuilder from "../../libs/QueryFilterBuilder";
 import { ApiResponse } from "../../libs/api/NetworkHelper";
+import Utils from "../../libs/Utils";
+import Enum from "../../libs/enums/Enum";
+import EvidenceType, { EvidenceTypeEnum } from "../../libs/enums/EvidenceType";
+import StatusType from "../../libs/enums/StatusType";
 
 const router = express.Router();
 
@@ -43,8 +47,56 @@ router.post("/", async (req, res) => {
 
 // @route    POST api/article/submit
 // @desc     Handles submission of a new article.
-router.post("/submit", async () => {
-  
+router.post("/submit", async (req, res) => {
+  const {
+    info
+  } = req.body;
+
+  try {
+    // Validate mandatory fields
+    if (!Utils.isValidString(info.TITLE)) {
+      throw new Error("Missing mandatory field - TITLE!");
+    }
+    if (!Utils.isValidString(info.AUTHOR)) {
+      throw new Error("Missing mandatory field - AUTHOR!");
+    }
+    if (!Utils.isValidString(info.SOURCE)) {
+      throw new Error("Missing mandatory field - SOURCE!");
+    }
+    if (!Utils.isValidString(info.type)) {
+      throw new Error("Missing mandatory field - type!");
+    }
+    if (!Utils.isValidNumber(info.YEAR)) {
+      throw new Error("Missing mandatory field - YEAR!");
+    }
+
+    // Make sure type is one of supported values.
+    const evidenceType = Enum.findByName(EvidenceType, info.type);
+    if (!(evidenceType instanceof EvidenceTypeEnum)) {
+      throw new Error(`Unsupported evidence type: ${info.type}`);
+    }
+
+    // Convert SOURCE appropriately based on type.
+    info[evidenceType.getFieldName()] = info.SOURCE;
+    delete info.SOURCE;
+
+    // Create article and insert to DB.
+    const newArticle = new Article({
+      submission: {
+        statusType: StatusType.submitted.name,
+        bibliography: info
+      }
+    });
+    await newArticle.save();
+
+    res.json(new ApiResponse({
+      isSuccess: true,
+      data: newArticle
+    }));
+  }
+  catch (e) {
+    res.status(401).json(new ApiResponse(e));
+  }
 });
 
 module.exports = router;
