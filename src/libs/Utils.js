452a24e30a150f0168cda7b3776bae1e0d182cf5
Jerry Kim <jerryrox@live.co.kr>
2020-06-11 16:30:22 +1200

    Added more utility functions

--- src/libs/Utils.js
+++ src/libs/Utils.js
@@ -1,12 +1,32 @@
 const Utils = {
 
-    createUUID() {
-        // Retrieved from https://stackoverflow.com/a/2117523
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-            const r = Math.random() * 16 | 0;
-            const v = (c === 'x' ? r : ((r & 0x3) | 0x8));
-            return v.toString(16);
-        });
+  createUUID() {
+    // Retrieved from https://stackoverflow.com/a/2117523
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      const r = Math.random() * 16 | 0;
+      const v = (c === 'x' ? r : ((r & 0x3) | 0x8));
+      return v.toString(16);
+    });
+  },
+
+  isValidString(value) {
+    if (value === null ||
+      value === undefined ||
+      typeof (value) !== "string" ||
+      value.length <= 0) {
+      return false;
     }
+    return true;
+  },
+
+  isValidNumber(value) {
+    if (value === null ||
+      value === undefined ||
+      typeof (value) !== "number" ||
+      isNaN(value)) {
+      return false;
+    }
+    return true;
+  }
 };
 export default Utils;
